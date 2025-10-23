72c79dfa387772fce46d6f890e2b569390c615c6
Pablo <pabloblaz06@gmail.com>
2025-10-23 19:25:21 +0200

    Cambios programacion en ejercicios

---
+++ src/Tema2/src/Ejercicios1/Basicos_6.java
@@ -0,0 +1,12 @@
+package Tema2.src.Ejercicios1;
+
+public class Basicos_6 {
+    public static void main(String[] args) {
+
+        int contador = 1;
+        while (contador <= 3) {
+            System.out.println(contador++);
+        }
+    }
+}
+
