b2a8673fb1c702d5f5e86a0934baae0f914f734e
pablo <pabloblaz06@gmail.com>
2025-11-12 13:59:34 +0100

    actualizaciones de los ejercicios de programacion

--- src/Tema2/src/Ejercicios4/Arryse0_3.java
+++ src/Tema2/src/Ejercicios4/Arryse0_3.java
@@ -9,9 +9,25 @@ public class Arryse0_3 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("cuantos datos vas a meter");
+        int cantidadDatos = sc.nextInt();
 
-        System.out.println("Cuantos datos vas a meter: ");
-        int num = sc.nextInt();
+        String[] nombres = new String[cantidadDatos];
+
+        int[] telefonos = new int[cantidadDatos];
+
+        for (int i = 0; i < cantidadDatos; i++) {
+            System.out.println("escribe el nombre");
+            String nombre = sc.next();
+            nombres[i] = nombre;
+
+            System.out.println("ahora escribe el numero");
+            telefonos[i] = sc.nextInt();
+
+            System.out.println("Nombre: " + nombres[i] + " Telefono: " + telefonos[i]);
+        }
+
+        sc.close();
 
         
     }
