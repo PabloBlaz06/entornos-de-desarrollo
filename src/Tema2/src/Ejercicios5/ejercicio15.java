632ac9d6fa0cde4baccf6736b375c9108b7cf0f0
pablo <pabloblaz06@gmail.com>
2026-01-09 13:57:46 +0100

    actualizaciones de los ejercicios de programacion

--- src/Tema2/src/Ejercicios5/ejercicio15.java
+++ src/Tema2/src/Ejercicios5/ejercicio15.java
@@ -11,8 +11,44 @@ Escribe una palabra: FIN
 La palabra con más vocales es: INVIERNO
 Y tiene 4 vocales */
 
+import java.util.Scanner;
+
 public class ejercicio15 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese palabra: ");
+        String palabra = sc.nextLine();
+
+        String palabraMax = " ";
+        int maxVocales = 0;
+
+        while (!palabra.equals("FIN")){
+
+            int vocales = 0;
+
+            for (int i = 0; i < palabra.length(); i++) {
+
+                char c = palabra.charAt(i);
+
+                if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u'
+                        || c=='A' || c=='E' || c=='I' || c=='O' || c=='U') {
+                    vocales++;
+                }
+            }
+
+            if (vocales > maxVocales) {
+                maxVocales = vocales;
+                palabraMax = palabra;
+            }
+
+            System.out.print("Ingrese palabra: ");
+            palabra = sc.nextLine();
+        }
+
+        System.out.println("La palabra con más vocales es: " + palabraMax);
+        System.out.println("Y tiene " + maxVocales + " vocales");
 
     }
 }
---
+++ src/Tema2/src/Ejercicios5/ejercicio16.java
@@ -0,0 +1,22 @@
+package Tema2.src.Ejercicios5;
+
+/*Realiza un programa que compruebe si en una palabra se repite alguna vocal más de 2 veces.
+El programa pedirá que se teclee una palabra y comprobará las vocales que contiene,
+cuando compruebe que una vocal aparece más de dos veces emitirá un mensaje en la pantalla y terminará.
+El programa debe terminar en el mismo instante en que encuentre más de dos vocales iguales,
+no esperará a comprobar toda la palabra (prohibido usar break).
+Ejemplo: Escriba una palabra: “Instintivo”
+Dará como resultado: “Tiene mas de dos vocales iguales” */
+
+import java.util.Scanner;
+
+public class ejercicio16 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese palabra: ");
+        String palabra = sc.nextLine();
+
+    }
+}
