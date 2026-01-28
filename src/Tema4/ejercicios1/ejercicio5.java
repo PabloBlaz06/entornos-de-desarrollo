117702b09e612c23a5bbe6026aca2d6cf2105dac
Pablo <pabloblaz06@gmail.com>
2026-01-28 16:59:50 +0100

    Cambios programacion en ejercicios

---
+++ src/Tema4/ejercicios1/ejercicio5.java
@@ -0,0 +1,10 @@
+package Tema4.ejercicios1;
+
+/*Escribe un programa que calcule cuántas horas y minutos dura una clase (o varias)
+ que introduzca el usuario*/
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+        
+    }
+}
