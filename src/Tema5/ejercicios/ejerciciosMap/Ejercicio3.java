7ba24f7cdcd128edf97a0bb442b05c98b512bc17
PB <pabloblaz06@gmail.com>
2026-03-16 18:54:46 +0100

    Cambios en los ejercicios de programacion

--- src/Tema5/ejercicios/ejerciciosMap/Ejercicio3.java
+++ src/Tema5/ejercicios/ejerciciosMap/Ejercicio3.java
@@ -6,13 +6,13 @@ import java.util.Map;
 public class Ejercicio3 {
     public static void main(String[] args) {
 
-        Map<String, Double> mapa = new HashMap<>();
-        mapa.put("Ruben" , 8.6);
-        mapa.put("Carlos" , 9.1);
-        mapa.put("Marcos" , 8.0);
-        mapa.put("Alejandro" , 7.2);
+        Map<String, Double> notas = new HashMap<>();
+        notas.put("Ruben" , 8.6);
+        notas.put("Carlos" , 9.1);
+        notas.put("Marcos" , 8.0);
+        notas.put("Alejandro" , 7.2);
 
-        for( Double nota : mapa.keySet()){
+        for( Double nota : notas.keySet()){
 
         }
     }
