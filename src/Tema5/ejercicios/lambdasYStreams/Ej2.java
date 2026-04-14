95db56c8e28680e402411a06e1113fcb0786a938
PB <pabloblaz06@gmail.com>
2026-04-14 19:27:36 +0200

    Cambios en los ejercicios de programacion

--- src/Tema5/ejercicios/lambdasYStreams/Ej2.java
+++ src/Tema5/ejercicios/lambdasYStreams/Ej2.java
@@ -1,5 +1,6 @@
 package Tema5.ejercicios.lambdasYStreams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,11 +8,11 @@ import java.util.stream.Collectors;
 public class Ej2 {
     public static void main(String[] args) {
 
-        List<String> alumnos = Arrays.asList("ana", "luis", "guzmán", "marta");
+        List<String> alumnos = new ArrayList<>(List.of("ana", "luis", "guzmán", "marta"));
 
         List<String> nomMayus = alumnos.stream()
-                .map(nombre -> nombre.toUpperCase())
-                .collect(Collectors.toList());
+                .map(String::toUpperCase) // Otra forma de ponerlo: .map(nombre -> nombre.toUpperCase())
+                .toList(); // Otra forma de ponerlo: .collect(Collectors.toList());
         nomMayus.forEach(System.out::println);
      }
 }
---
+++ src/tema6/practicaClase/ejercicioFichero/Ej1.java
@@ -0,0 +1,27 @@
+package tema6.practicaClase.ejercicioFichero;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ej1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un nombre de un archivo: ");
+        String nombre = sc.nextLine();
+        sc.close();
+
+        try {
+                File file = new File(nombre);
+            if(!file.exists())
+                file.createNewFile();
+            else
+                System.out.println(file.getAbsolutePath());
+        } catch (IOException e) {
+            System.out.println("Error al crear el archivo, " + e.getMessage());
+        }
+
+    }
+}
